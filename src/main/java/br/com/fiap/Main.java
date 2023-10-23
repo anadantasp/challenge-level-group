922102a9d7fd3953c3c7feb0a45b43f0f1e24a21
anadantasp <caroldantasp@gmail.com>
2023-10-23 16:41:13 -0300

    Atualizando atributos da requisição

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -5,6 +5,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import javax.swing.*;
+import java.time.LocalDate;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -13,42 +17,41 @@ public class Main {
 
         EntityManager manager = factory.createEntityManager();
 
-        cadastroDados(manager);
+        //cadastroDados(manager);
 
-        //buscarInventarioPorId(manager);
+        buscarRequisicaoPorId(manager);
 
 
-        //buscarTodosBens(manager);
+        //buscarTodasRequisições(manager);
 
         manager.close();
         factory.close();
 
 
     }
-/*
-    private static void buscarTodosBens(EntityManager manager) {
-        List<Bem> bens = manager.createQuery("FROM Bem").getResultList();
 
-        bens.forEach(System.out::println);
+    private static void buscarTodasRequisições(EntityManager manager) {
+        List<Requisicao> requisicoes = manager.createQuery("FROM Requisicao").getResultList();
+
+        requisicoes.forEach(System.out::println);
     }
 
-    private static void buscarInventarioPorId(EntityManager manager) {
-        Long id = Long.valueOf(JOptionPane.showInputDialog("ID do inventário: "));
-        Inventario inventario = manager.find(Inventario.class, id);
-        System.out.println(inventario);
+    private static void buscarRequisicaoPorId(EntityManager manager) {
+        Long id = Long.valueOf(JOptionPane.showInputDialog("ID da Requisição: "));
+        Requisicao requisicao = manager.find(Requisicao.class, id);
+        System.out.println(requisicao);
     }
-*/
+
     private static void cadastroDados(EntityManager manager) {
-        Empresa empresa = new Empresa(null, "Flor de Mim", "15683171000130", "04005060");
-        Funcionario funcionario = new Funcionario(null, "Yasmim", "yasmim@email.com.br", "senha123", empresa );
+        Empresa empresa = new Empresa(null, "Flor de Mim", "95586516000176", "04005060");
+        Funcionario funcionario = new Funcionario(null, "Yasmim", "yasmim2@email.com.br", "senha123", empresa );
 
         Produto produto = new Produto(null, "Planner do Ano", "19x15");
         Estoque estoque = new Estoque(null, 150, 800, 150, empresa, produto);
         ItemPedido itemPedido = new ItemPedido(null, estoque);
-        FormaPagamento formaPagamento = new FormaPagamento(null, "debito", "à vista");
-        Requisicao requisicao = new Requisicao(null, null);
+        FormaPagamento formaPagamento = new FormaPagamento(null, "credito", "à vista");
+        Requisicao requisicao = new Requisicao(null, LocalDate.now(), null, "Solicitado", null, formaPagamento);
         requisicao.addItem(itemPedido);
-        requisicao.setFormaPagamento(formaPagamento);
 
         manager.getTransaction().begin();
         manager.persist(funcionario);
--- src/main/java/br/com/fiap/domain/entity/Requisicao.java
+++ src/main/java/br/com/fiap/domain/entity/Requisicao.java
@@ -2,6 +2,7 @@ package br.com.fiap.domain.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -15,6 +16,15 @@ public class Requisicao {
     @SequenceGenerator(name = "SQ_REQUISICAO", sequenceName = "SQ_REQUISICAO")
     @Column(name = "ID_REQUISICAO")
     private Long id;
+
+    @Column(name = "DT_ABERTURA_REQUISICAO")
+    private LocalDate dtAbertura;
+
+    @Column(name = "DT_ENCERRAMENTO_REQUISICAO")
+    private LocalDate dtEncerramento;
+
+    @Column(name = "STATUS_REQUISICAO")
+    private String status;
 
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
@@ -53,12 +63,48 @@ public class Requisicao {
         this.itensPedido = Objects.nonNull(itensPedido) ? itensPedido : new LinkedHashSet<>();
     }
 
+    public Requisicao(Long id, LocalDate dtAbertura, LocalDate dtEncerramento, String status, Set<ItemPedido> itensPedido, FormaPagamento formaPagamento) {
+        this.id = id;
+        this.dtAbertura = dtAbertura;
+        this.dtEncerramento = dtEncerramento;
+        this.status = status;
+        this.itensPedido = Objects.nonNull(itensPedido) ? itensPedido : new LinkedHashSet<>();
+        this.formaPagamento = formaPagamento;
+    }
+
     public Long getId() {
         return id;
     }
 
     public Requisicao setId(Long id) {
         this.id = id;
+        return this;
+    }
+
+    public LocalDate getDtAbertura() {
+        return dtAbertura;
+    }
+
+    public Requisicao setDtAbertura(LocalDate dtAbertura) {
+        this.dtAbertura = dtAbertura;
+        return this;
+    }
+
+    public LocalDate getDtEncerramento() {
+        return dtEncerramento;
+    }
+
+    public Requisicao setDtEncerramento(LocalDate dtEncerramento) {
+        this.dtEncerramento = dtEncerramento;
+        return this;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Requisicao setStatus(String status) {
+        this.status = status;
         return this;
     }
 
@@ -85,12 +131,14 @@ public class Requisicao {
         return this;
     }
 
-
     @Override
     public String toString() {
         return "Requisicao{" +
                 "id=" + id +
-                //", itensPedido=" + itensPedido +
+                ", dtAbertura=" + dtAbertura +
+                ", dtEncerramento=" + dtEncerramento +
+                ", status='" + status + '\'' +
+                ", itensPedido=" + itensPedido +
                 ", formaPagamento=" + formaPagamento +
                 '}';
     }
